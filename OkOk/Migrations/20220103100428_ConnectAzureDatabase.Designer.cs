6406b683a02420a8aac607feb4adac2fa62bd66a
Dechaun <64783977+dechaunb@users.noreply.github.com>
2022-01-03 11:06:02 +0100

    made connection

--- OkOk/Migrations/20220103100428_ConnectAzureDatabase.Designer.cs
+++ OkOk/Migrations/20220103100428_ConnectAzureDatabase.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OkOk.Data;
 
@@ -10,9 +11,10 @@ using OkOk.Data;
 namespace OkOk.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220103100428_ConnectAzureDatabase")]
+    partial class ConnectAzureDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.1");
