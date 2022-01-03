9bb46813a5b675aea3f2eb0a0c927579b3ec35c2
Dechaun <64783977+dechaunb@users.noreply.github.com>
2022-01-03 11:41:43 +0100

    created base classes

---
+++ OkOk/APIs/ZorgdomeinService.cs
@@ -0,0 +1,17 @@
+using Newtonsoft.Json;
+
+namespace OkOk.APIs
+{
+    public class ZorgdomeinService
+    {
+        static HttpClient client = new HttpClient();
+
+        // static async Task RunAsync()
+        // {
+        //     // Update port # in the following line.
+        //     client.BaseAddress = new Uri("http://localhost:64195/");
+        //     client.DefaultRequestHeaders.Accept.Clear();
+        //     client.DefaultRequestHeaders.Accept.Add(
+        //         new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+}
