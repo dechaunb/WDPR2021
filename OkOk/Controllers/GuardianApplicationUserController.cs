a3e3e0b131127dcf8c054009c05471c7913ff412
TimothyTF <timothytjien@hotmail.com>
2022-01-06 17:51:17 +0100

    Index view aangemaakt voor Guardian

---
+++ OkOk/Controllers/GuardianApplicationUserController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OkOk.Data;
+using OkOk.Models.Identity;
+using OkOk.Models;
+
+namespace OkOk.Controllers
+{
+    public class GuardianApplicationUserController : Controller{
+        private readonly ApplicationDbContext _context;
+
+        public GuardianApplicationUserController(ApplicationDbContext context){
+            _context = context;
+        }
+
+        [Route("/Guardian/Index")]
+        public IActionResult Index(){
+            GuardianApplicationUser g = new GuardianApplicationUser(){
+                FirstName = "Gerrit",
+                LastName = "A",
+                Children = new List<ClientApplicationUser>(){
+                    new ClientApplicationUser(){
+                        FirstName = "Arie",
+                        LastName = "A",
+                        Address = new Address(){
+                            HouseNumber = 1,
+                            Street = "Straat",
+                            City = "Delft",
+                            ZipCode = "2121DW",
+                            Country = "Nederland"
+                        }
+                    },
+                    new ClientApplicationUser(){
+                        FirstName = "Juliët",
+                        LastName = "A",
+                        Address = new Address(){
+                            HouseNumber = 1,
+                            Street = "Straat",
+                            City = "Delft",
+                            ZipCode = "2121DW",
+                            Country = "Nederland"
+                        }
+                    }
+                }
+            };
+            //_context.GuardianApplicationUsers.Add(g);
+            _context.SaveChanges();
+
+            foreach (var guardian in _context.GuardianApplicationUsers)
+            {
+                Console.WriteLine(guardian.FirstName);
+            }
+
+            //ViewData["Children"] = _context.GuardianApplicationUsers;
+            return View(g);
+        }
+
+        //[Route("/GuardianApplicationUser/ChildChatFrequency/{id}")]
+        public IActionResult ChildChatFrequency(string id){
+            ViewData["GuardianChild"] = _context.ClientApplicationUsers.Where(c => c.Id == id).FirstOrDefault();
+            return View();
+        }
+    }
+}
