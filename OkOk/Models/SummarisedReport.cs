ac6f4492a21c038484f1ab018cf307edd155ab4a
Angelo Heringa <48793053+AngeloHeringa@users.noreply.github.com>
2022-01-18 11:33:05 +0100

    SummarisedReport GUID fix

--- OkOk/Models/SummarisedReport.cs
+++ OkOk/Models/SummarisedReport.cs
@@ -2,7 +2,7 @@ namespace OkOk.Models
 {
     public class SummarisedReport
     {
-        public Guid MessageId{get; set;}
+        public Guid? MessageId{get; set;}
         public string SenderId{get; set;}
         public int Amount{get; set;}
         public string Content{get; set;}
